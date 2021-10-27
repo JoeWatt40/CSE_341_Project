05854b0b1e69bf159c3179c2dc5199ddce8e7816
joewatt40 <wat19030@byui.edu>
2021-10-26 23:20:00 -0700

    updating npm installs

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-/*******************************************************************************
- * Feel free to remove this comment block and all other comments after pulling.
- * They're for information purposes only.
- *
- * This layout is provided to you for an easy and quick setup to either pull
- * or use to correct yours after working at least 1 hour on Team Activity 02.
- * Throughout the course, we'll be using Express.js for our view engines.
- * However, feel free to use pug or handlebars ('with extension hbs'). You will
- * need to make sure you install them beforehand according to the reading from
- * Udemy course.
- * IMPORTANT: Make sure to run "npm install" in your root before "npm start"
- *******************************************************************************/
 // Our initial setup (package requires, port number setup)
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -28,11 +16,6 @@ app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  // For view engine as Pug
-  //.set('view engine', 'pug') // For view engine as PUG.
-  // For view engine as hbs (Handlebars)
-  //.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'})) // For handlebars
-  //.set('view engine', 'hbs')
   .use(bodyParser({ extended: false })) // For parsing the body of a POST
   .use('/ta01', ta01Routes)
   .use('/ta02', ta02Routes)
